56d9ea0325b3e35260463961a6bc04ceece83bcb
Dierk Koenig <dierk.koenig@canoo.com>
2015-07-03 15:28:39 +0200

    housekeeping, cleaning up warnings, make better use of type variables in native declarations

--- fregeFX/src/main/java/org/frege/Classifier.java
+++ fregeFX/src/main/java/org/frege/Classifier.java
@@ -10,7 +10,7 @@ public class Classifier {
     static public org.frege.AllNodes.TAllNodes classify(Node n) throws IllegalArgumentException {
         if (n instanceof Button)   return org.frege.AllNodes.TAllNodes.DButton.mk((Button) n);
         if (n instanceof TextArea) return org.frege.AllNodes.TAllNodes.DTextArea.mk((TextArea) n);
-        if (n instanceof ListView) return org.frege.AllNodes.TAllNodes.DListView.mk((ListView) n);
+        if (n instanceof ListView) return org.frege.AllNodes.TAllNodes.DListView.mk((ListView) n); 
         System.out.println("Cannot cast node " + n.getClass() + " to a known type");
         throw new IllegalArgumentException("Cannot cast node " + n.getClass() + " to a known type");
     }
