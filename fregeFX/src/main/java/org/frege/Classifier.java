ca549ed76854d57566f660704e8415aaa19825c9
Dierk Koenig <dierk.koenig@canoo.com>
2015-08-20 21:57:41 +0200

    backup before major cleanup

--- fregeFX/src/main/java/org/frege/Classifier.java
+++ fregeFX/src/main/java/org/frege/Classifier.java
@@ -7,11 +7,11 @@ import javafx.scene.control.ListView;
  
 public class Classifier {
 
-    static public org.frege.AllNodes.TAllNodes classify(Node n) throws IllegalArgumentException {
-        if (n instanceof Button)   return org.frege.AllNodes.TAllNodes.DButton.mk((Button) n);
-        if (n instanceof TextArea) return org.frege.AllNodes.TAllNodes.DTextArea.mk((TextArea) n);
-        if (n instanceof ListView) return org.frege.AllNodes.TAllNodes.DListView.mk((ListView) n); 
-        System.out.println("Cannot cast node " + n.getClass() + " to a known type");
-        throw new IllegalArgumentException("Cannot cast node " + n.getClass() + " to a known type");
-    }
+//    static public org.frege.AllNodes.TAllNodes classify(Node n) throws IllegalArgumentException {
+//        if (n instanceof Button)   return org.frege.AllNodes.TAllNodes.DButton.mk((Button) n);
+//        if (n instanceof TextArea) return org.frege.AllNodes.TAllNodes.DTextArea.mk((TextArea) n);
+//        if (n instanceof ListView) return org.frege.AllNodes.TAllNodes.DListView.mk((ListView) n);
+//        System.out.println("Cannot cast node " + n.getClass() + " to a known type");
+//        throw new IllegalArgumentException("Cannot cast node " + n.getClass() + " to a known type");
+//    }
 }
